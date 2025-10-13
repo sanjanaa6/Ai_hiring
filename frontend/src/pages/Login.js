756a33fa12a7c9b766fcfb40ebd210dcf384ed7b
sanjanaa6 <sanjanaasingh.05@gmail.com>
2025-10-13 11:19:25 +0530

    changes

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -155,7 +155,7 @@ const Login = () => {
             className="mb-6"
           >
             <button
-              onClick={() => window.open(window.location.origin, '_blank')}
+              onClick={() => window.location.href = window.location.origin}
               className="flex items-center text-blue-400 hover:text-blue-300 transition-colors duration-200 group"
             >
               <ArrowLeft className="w-5 h-5 mr-2 group-hover:-translate-x-1 transition-transform duration-200" />
